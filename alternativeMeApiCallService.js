51b761cadaa62f7e44a816db8e70187aacba805d
Thomas Starzynski <thomas.starzynski@unibas.ch>
2019-07-03 21:54:52 +0200

    refactored coincard creation

--- alternativeMeApiCallService.js
+++ alternativeMeApiCallService.js
@@ -1,15 +1,11 @@
 // URL GRABBER -----------------------------------------------------------------
 const getUrlSuffix = url => {
-  console.log("URL:");
-  console.log(url);
   let suffix;
   if (url.includes("?")) {
     suffix = url.split("?")[1];
   } else {
     suffix = "BTC=1&ETH=1&XRP=1&LTC=1&BCH=1&EOS=1&BNB=1&BSV=1&USDT=1&XLM=1&ADA=1&TRX=1&XMR=1&DASH=1&MIOTA=1&NEO=1&ETC=1&XTZ=1&XEM=1&MKR=1&ONT=1&ZEC=1&LINK=1&BTG=1&BAT=1&DOGE=1&QTUM=1&OMG=1&BTT=1&DCR=1&TUSD=1&HOT=1&LSK=1&WAVES=1&RVN=1&BCD=1&NANO=1&NPXS=1&ZIL=1&ZRX=1&REP=1&KMD=1&ICX=1&BCN=1&BTM=1&BTS=1&HT=1&DGB=1&DENT=1&AE=1&XVG=1&IOST=1&SC=1&ETP=1&MONA=1&STEEM=1&THETA=1&ENJ=1&ELF=1&ARDR=1&SNT=1&MCO=1&GNT=1&XIN=1&NAS=1&NULS=1&ZEN=1&ARK=1&DGD=1&LOOM=1";
   }
-  console.log("SUFFIX:");
-  console.log(suffix);
   return suffix;
 };
 // converts params string to userPortfolio object
@@ -23,16 +19,6 @@ const convertUrlSuffix = urlSuffix => {
   });
   return result
 };
-// GET ALL COINS CODE SNIPPET
-// const url = window.location.href.split(':8080')[0] + ':8080/crypto-mockup-data.json';
-// fetch(url)
-// .then((resp) => resp.json())
-// .then(function(result) {
-//   console.log(result);
-//   let coins = Object.values(result.data);
-//   coins = coins.map(coin => `${coin.symbol}=1`);
-//   console.log(coins.join("&"));
-// })
 
 
 
@@ -56,7 +42,7 @@ const alternativeMeApiCall = () => {
     buildCoinCards(allCoins);
   })
   .catch(function(error){
-    console.log("catching...");
+    console.log("catching error...");
     console.log(JSON.stringify(error));
   });
 }
@@ -83,7 +69,7 @@ const computePortfolioValue = () => {
 
 
 
-// BUILD CARDS -----------------------------------------------------------------
+// NORMALIZER HELP FUNCTIONS----------------------------------------------------
 const normalizeValue = (totalValue) => {
   return totalValue.toFixed(2).toString().replace(/\B(?=(\d{3})+(?!\d))/g, "'")
 };
@@ -101,99 +87,40 @@ const normalizePrice = (price) => {
     return price.toFixed(2)
   }
 };
-const buildCoinCards = (coins) => {
-  // for each userPortfolio -> find data from fetched coins array
-  userPortfolio.forEach((userCoin) => {
-    // create card
-    const coinCard = buildCoinCard(userCoin, coins);
-    // append card to container
-  });
-  // });
-};
-const buildCoinCard = (userCoin, coins) => {
-  let target = coins.find((element) => {
-    return element.symbol === userCoin.name;
-  });
-  // TARGET
-  // <div class="coin-card">
-  //   <div class="icon">
-  //     <div class="icon-flex">
-  //       <img src="images/btc.svg" alt="">
-  //       <div class="percentage">74%</div>
-  //     </div>
-  //   </div>
-  //   <div class="coin-info">
-  //     <p class="coin-name">Bitcoin</p>
-  //     <p class="coin-price">7'256.20 €</p>
-  //   </div>
-  //   <div class="holdings-info">
-  //     <p class="holdings-value">36'281.20 €</p>
-  //     <p class="holdings-amount">5 BTC</p>
-  //   </div>
-  // </div>
 
-  // create coin-card div
-  const coinCard = document.createElement('div');
-  coinCard.classList.add("coin-card");
 
-  //----------------------
-  // create icon div
-  const icon = document.createElement('div');
-  icon.classList.add("icon");
-  //   create icon-flex div
-  const iconFlex = document.createElement('div');
-  iconFlex.classList.add("icon-flex");
-  //     create img
-  const img = document.createElement('img');
-  img.src = `images/svg/color/${target.symbol.toLowerCase()}.svg`
-  //     create percentage div
-  const percentage = document.createElement('div');
-  percentage.classList.add("percentage");
-  console.log((target.price * userCoin.quantity));
-  percentage.innerText = `${normalizePercentage(target.price * 100 * userCoin.quantity / portfolioValueFloat)}%`;
 
-  //----------------------
-  // create coin-info div
-  const coinInfo = document.createElement('div');
-  coinInfo.classList.add("coin-info");
-  //   create coin-name p
-  const coinName = document.createElement('p');
-  coinName.classList.add("coin-name");
-  coinName.innerText = target.name;
-  //   create coin-price p
-  const coinPrice = document.createElement('p');
-  coinPrice.classList.add("coin-price");
-  coinPrice.innerText = `${normalizePrice(target.price)} $`;
 
-  //----------------------
-  // create holdings-info div
-  const holdingsInfo = document.createElement('div');
-  holdingsInfo.classList.add("holdings-info");
-  // create holding-value p
-  const holdingsValue = document.createElement('p');
-  holdingsValue.classList.add("holdings-value");
-  holdingsValue.innerText = `${normalizeValue(target.price * userCoin.quantity)} $`;
-  // create holding-amount p
-  const holdingsAmount = document.createElement('p');
-  holdingsAmount.classList.add("holdings-amount");
-  holdingsAmount.innerText = `${userCoin.quantity} ${target.symbol}`;
 
-  // append
-  iconFlex.appendChild(img);
-  iconFlex.appendChild(percentage);
-  icon.appendChild(iconFlex);
-
-  coinInfo.appendChild(coinName);
-  coinInfo.appendChild(coinPrice);
-
-  holdingsInfo.appendChild(holdingsValue);
-  holdingsInfo.appendChild(holdingsAmount);
-
-  coinCard.appendChild(icon);
-  coinCard.appendChild(coinInfo);
-  coinCard.appendChild(holdingsInfo);
-
-  coinCardsContainer.appendChild(coinCard);
+// BUILD CARDS -----------------------------------------------------------------
+const buildCoinCards = coins => userPortfolio.forEach(userCoin => buildCoinCard(userCoin, coins));
+const buildCoinCard  = (userCoin, coins) => {
+  const target         = coins.find(element => element.symbol === userCoin.name);
+  const svgPath        = `images/svg/color/${target.symbol.toLowerCase()}.svg`;
+  const percentage     = `${normalizePercentage(target.price * 100 * userCoin.quantity / portfolioValueFloat)}%`;
+  const coinName       = target.name;
+  const coinPrice      = `${normalizePrice(target.price)} $`;
+  const holdingsValue  = `${normalizeValue(target.price * userCoin.quantity)} $`;
+  const holdingsAmount = `${userCoin.quantity} ${target.symbol}`;
+  const innerHTML = `
+    <div class="coin-card">
+      <div class="icon">
+        <div class="icon-flex">
+          <img src="${svgPath}" alt="">
+          <div class="percentage">${percentage}</div>
+        </div>
+      </div>
+      <div class="coin-info">
+        <p class="coin-name">${coinName}</p>
+        <p class="coin-price">${coinPrice}</p>
+      </div>
+      <div class="holdings-info">
+        <p class="holdings-value">${holdingsValue}</p>
+        <p class="holdings-amount">${holdingsAmount}</p>
+      </div>
+    </div>
+  `;
+  coinCardsContainer.insertAdjacentHTML("beforeend", innerHTML);
 };
 
 
